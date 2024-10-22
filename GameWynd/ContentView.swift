6ac3e9df606e3e9a030ee662ca6902652061f5d2
RanaAk <mahediAkbar02@gmail.com>
2024-10-22 22:57:52 +0600

    Initial Commit

---
+++ GameWynd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GameWynd
+//
+//  Created by RANA  on 22/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameWynd/GameWyndApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameWyndApp.swift
+//  GameWynd
+//
+//  Created by RANA  on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GameWyndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
