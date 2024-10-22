8b949a9269a4a61a39233cf0761e3279e90649a2
RanaAk <mahediAkbar02@gmail.com>
2024-10-22 23:57:27 +0600

    Create folders for models, components, and views; add GameDetailView and Home files

---
+++ GameWynd/Views/GameDetailView.swift
@@ -0,0 +1,107 @@
+//
+//  GameDetailView.swift
+//  TestGame
+//
+//  Created by RANA  on 21/10/24.
+//
+
+import SwiftUI
+
+struct GameDetailView: View {
+    var game : GameModel
+    @Environment(\.dismiss) private var dismiss
+    @State private var playGame : Bool = false
+    var body: some View {
+        NavigationStack{
+            VStack {
+                Image(game.image)
+                    .resizable()
+                    .frame(width: 400, height: 350)
+                    .toolbar(content: {
+                        ToolbarItem(placement: .topBarLeading) {
+                            Button(action: {
+                                dismiss()
+                            }, label: {
+                                Image(systemName: "arrowshape.backward.fill")
+                                    .imageScale(.large)
+                                    .foregroundStyle(.white)
+                            })
+                           
+                            
+                            
+                        }
+                    })
+                   
+                    
+                
+                Text(game.name)
+                    .font(.title2)
+                    .foregroundStyle(.white.gradient)
+                    .padding()
+                    .background(Color(.systemGray2))
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                
+                VStack(alignment : .leading , spacing: 10){
+                    
+                    Text("Descriptions")
+                        .foregroundStyle(.white)
+                        .bold()
+                        .frame(width: 300, alignment: .leading)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 15).fill(.blue))
+                        .padding([.top, .leading])
+                    
+                    Text(game.caption)
+                        .padding(.leading)
+                    
+                }
+                  
+                Button(action: {
+                    playGame.toggle()
+                }, label: {
+                    Image(systemName: "play")
+                        .imageScale(.large)
+                        .bold()
+                        .padding()
+                        .foregroundStyle(.white)
+                        .frame(width: 300)
+                })
+                .background(RoundedRectangle(cornerRadius: 20).fill(.cyan.gradient))
+                .padding(.top)
+                .fullScreenCover(isPresented: $playGame, onDismiss: {
+                    // Code to run on dismiss
+                }, content: {
+                    ZStack {
+                        WebView(url: URL(string: game.link))
+                            .ignoresSafeArea()
+                        
+                        VStack {
+                            HStack {
+                                Spacer()
+                                Button(action: {
+                                    playGame = false
+                                }) {
+                                    Image(systemName: "xmark.circle.fill")
+                                        .foregroundStyle(.black.gradient)
+                                        .font(.title)
+                                        .padding()
+                                }
+                            }
+                            Spacer()
+                        }
+                    }
+                })
+
+                
+                Spacer()
+            }
+
+            .ignoresSafeArea()
+        }
+        
+    }
+}
+
+#Preview {
+    GameDetailView(game: gameList[0])
+}
---
+++ GameWynd/Views/Home.swift
@@ -0,0 +1,79 @@
+//
+//  Home.swift
+//  TestGame
+//
+//  Created by RANA  on 20/10/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        NavigationStack {
+            HeaderView()
+            
+            VStack {
+                GeometryReader{ proxy in
+                    let size = proxy.size
+                    
+                    ScrollView{
+                        ScrollView(.horizontal) {
+                            
+                            LazyHStack {
+                                ForEach(gameList){ game in
+                                    NavigationLink {
+                                        GameDetailView(game: game)
+                                            .navigationBarBackButtonHidden()
+                                    } label: {
+                                        Image(game.image)
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: size.width-40, height: size.width-20, alignment: .leading)
+                                        
+                                            .overlay(alignment: .bottom, content: {
+                                                PlayInOverLay(game: game)
+                                            })
+                                            .clipShape(.rect(cornerRadius: 18))
+                                            .shadow(color: .black.opacity(0.15), radius: 8, x: 5, y: 10)
+                                            .padding(.leading, 20)
+                                    }
+                                   
+                                    
+                                }
+                                .scrollTransition(.interactive, axis: .horizontal) { view, phase in
+                                    view.scaleEffect(phase.isIdentity ? 1 : 0.7)
+                                    //view.offset(x : phase.value * -250)
+                                }
+                                
+                                
+                            }
+                            
+                            .padding([.top, .bottom], 15)
+                            //.padding(.leading , 0)
+                            .scrollTargetLayout()
+                            
+                        }
+                        
+                        .scrollTargetBehavior(.viewAligned)
+                        .scrollIndicators(.hidden)
+                        
+                        Text("All Games")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                        myGridView(size: size.width)
+                        
+                        //.padding(.horizontal, 20) // Padding for the grid container
+                        
+                        
+                    }
+                }
+            }
+        }
+        .fontDesign(.monospaced)
+    }
+    
+}
+
+#Preview {
+    Home()
+}
